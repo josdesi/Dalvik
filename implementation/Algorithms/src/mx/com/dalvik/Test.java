318185be7ddeae32d92dc69576d577a9c9aeff47
josdesi <josdesi@gmail.com>
2018-09-05 09:00:35 -0500

    GaussianElimination

--- implementation/Algorithms/src/mx/com/dalvik/Test.java
+++ implementation/Algorithms/src/mx/com/dalvik/Test.java
@@ -1,6 +1,10 @@
 package mx.com.dalvik;
 
 import mx.com.dalvik.model.dice.IDiceSimulator;
+
+import java.text.NumberFormat;
+
+import mx.com.dalvik.model.GaussianElimination;
 import mx.com.dalvik.model.dice.DiceSimulatorOptimized;
 import mx.com.dalvik.model.dice.DiceSimulatorSimple;
 import mx.com.dalvik.model.dice.Logger;
@@ -9,7 +13,55 @@ public class Test {
 
 	
 	public static void main(String[] args) {
-		System.out.println("...");
+		double [][] matrix = {	{1,5,-2},
+								{2,3,1},
+								{2,4,-3}};
+		double [] vector = {2,5,2};
+		
+		GaussianElimination gauss = new GaussianElimination();
+		double [] solve = gauss.solve(matrix, vector);
+		
+		System.out.println("\n\nVector solution:");
+		for( int i = 0; i< solve.length; i++ )
+			System.out.println( solve[ i ] );
+		
+	}
+	
+	
+	public static void mainNumberPrecision(String[] args) {
+		
+		double num = 10;
+		double sum = 0;
+		
+		for( int i = 0; i< 100; i++ )
+			sum += 0.1;
+	
+		System.out.println( num == sum );
+		System.out.println( sum );
+		
+		double epsilon = 0.0000000001;
+		
+		if( Math.abs( sum - num ) < epsilon )
+			System.out.println("They are equal");
+		else
+			System.out.println("Not equal");
+		
+	}
+	
+	
+	public static void mainNumberFormat(String[] args) {
+		
+		// IEEE 754
+		double num = 0.8;
+		double num2 = 0.4;
+		double res = num + num2;
+		
+		System.out.println( res );
+		
+		NumberFormat nf = NumberFormat.getInstance();
+		nf.setMaximumFractionDigits(3);
+		System.out.println("res: "+ nf.format( res ));
+		
 	}
 	
 	
