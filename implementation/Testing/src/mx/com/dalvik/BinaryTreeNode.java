e10aa8ade3a1c2e95b3d4e45f44e41512240bac6
josdesi <josdesi@gmail.com>
2018-09-10 08:22:09 -0500

    Clases estereotipadas, clases abstractas y clases autoreferenciadas.

---
+++ implementation/Testing/src/mx/com/dalvik/BinaryTreeNode.java
@@ -0,0 +1,14 @@
+package mx.com.dalvik;
+
+import java.util.Vector;
+
+public class BinaryTreeNode {
+	
+	private BinaryTreeNode leftNode;
+	private BinaryTreeNode rightNode;
+	
+	private BinaryTreeNode _nodos[] = new BinaryTreeNode[ 2 ];
+	private BinaryTreeNode nodos[] = new BinaryTreeNode[ 200 ];
+	
+	private Vector<BinaryTreeNode> lista;
+}
---
+++ implementation/Testing/src/mx/com/dalvik/Shape.java
@@ -0,0 +1,8 @@
+package mx.com.dalvik;
+
+public abstract class Shape {
+	
+	private Object itsAnchorPoint;
+	
+	public abstract void draw();
+}
