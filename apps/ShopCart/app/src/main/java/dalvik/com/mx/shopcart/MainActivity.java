54adf2d5c1fbf38e04e8de50c3c0beacb4721ae3
josdesi <josdesi@gmail.com>
2018-09-14 08:44:14 -0500

    Agregar SignIn activity

--- apps/ShopCart/app/src/main/java/dalvik/com/mx/shopcart/MainActivity.java
+++ apps/ShopCart/app/src/main/java/dalvik/com/mx/shopcart/MainActivity.java
@@ -1,7 +1,10 @@
 package dalvik.com.mx.shopcart;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+    }
+
+    private void init() {
+        Button btnSignIn = findViewById(R.id.btnStart );
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent( MainActivity.this, SignInActivity.class );
+                startActivity( intent );
+            }
+        });
     }
 }
