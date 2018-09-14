e6039e93ed2df2546e7e85ca43dc30ed3f033c3f
josdesi <josdesi@gmail.com>
2018-09-14 07:57:30 -0500

    Clase interna anónima.

---
+++ implementation/Testing/src/mx/com/dalvik/MyWindow.java
@@ -0,0 +1,14 @@
+package mx.com.dalvik;
+
+public class MyWindow {
+	
+	public void f() {
+		ActionListener l = new ActionListener() {
+			// ... 
+		};
+	}
+	
+	interface ActionListener{
+		
+	}
+}
