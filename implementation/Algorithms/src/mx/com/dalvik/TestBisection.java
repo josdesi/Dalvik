ff2ca3dd3c7b10c62bf1e460d86c11580aa25d1f
josdesi <josdesi@gmail.com>
2018-09-18 08:16:40 -0500

    BisectionMethod

---
+++ implementation/Algorithms/src/mx/com/dalvik/TestBisection.java
@@ -0,0 +1,16 @@
+package mx.com.dalvik;
+
+import mx.com.dalvik.model.BisectionMethod;
+
+public class TestBisection {
+	
+	public static void main(String[] args) {
+		
+		
+		BisectionMethod bm = new BisectionMethod();
+		System.out.println( bm.find(0,4));
+		
+		System.out.println( Math.sqrt(2) );
+		
+	}
+}
