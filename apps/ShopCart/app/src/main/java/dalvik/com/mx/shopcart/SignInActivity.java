fc2cd684b14ddad4e039ea48f3fba37768d481fc
josdesi <josdesi@gmail.com>
2018-09-19 08:54:35 -0500

    Home Activity & textPassword.

--- apps/ShopCart/app/src/main/java/dalvik/com/mx/shopcart/SignInActivity.java
+++ apps/ShopCart/app/src/main/java/dalvik/com/mx/shopcart/SignInActivity.java
@@ -1,8 +1,11 @@
 package dalvik.com.mx.shopcart;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
@@ -27,7 +30,22 @@ public class SignInActivity extends AppCompatActivity {
 
         mTextView = findViewById( R.id.txtMsg );
 
+        init();
     }
+
+    private void init() {
+
+        Button btnSignIn = findViewById(R.id.btnLogin );
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent( SignInActivity.this, HomeActivity.class );
+                startActivity( intent );
+            }
+        });
+    }
+
     @Override
     protected void onStart() {
 
