61a196f6d54abc6f130c4642eeed156f88129371
josdesi <josdesi@gmail.com>
2018-09-28 08:35:24 -0500

    Testing Gaussian & Regression.

--- implementation/Algorithms/src/mx/com/dalvik/TestGaussian.java
+++ implementation/Algorithms/src/mx/com/dalvik/TestGaussian.java
@@ -5,15 +5,19 @@ import mx.com.dalvik.model.GaussianElimination;
 public class TestGaussian {
 	
 	public static void main(String[] args) {
-		double [][] matrix = {	{1,1,1,1},
-								{1,5,25,125},
-								{1,20,400,8000},
-								{1,40,1600,64000}};
+//		double [][] matrix = {	{1,1,1,1},
+//								{1,5,25,125},
+//								{1,20,400,8000},
+//								{1,40,1600,64000}};
+//		
+//		double [] vector = {56.5,113,181,214.5};
 		
-		double [] vector = {56.5,113,181,214.5};
 		
-		
-		
+		double [][] matrix = {{1,1},{30,900}};
+
+		double [] vector = {2,0};
+
+
 		
 		
 		
